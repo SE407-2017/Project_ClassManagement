58bc5c699fc67a4628b6340dee8327798ab7f195
LyuJ1998 <31683380+LyuJ1998@users.noreply.github.com>
2017-10-24 16:06:03 +0800

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'class_management',
+    'class_management',########################should be added
 ]
 
 MIDDLEWARE = [
