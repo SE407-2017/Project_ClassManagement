2ff5f9a3d923807a211c58acd860f14642fba90a
CatchFishTail <610228633@qq.com>
2017-11-05 14:26:39 +0800

    Addclass
    Showimages

--- class_management/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class ClassManagementConfig(AppConfig):
-    name = 'class_management'
---
+++ project/class_management/views.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponse
+from django import forms
+from models import theUser
+
+class UserFormForRegist(forms.Form):
+    username = forms.CharField(label='用户名',max_length=50)
+    password = forms.CharField(label='密码',widget=forms.PasswordInput())
+    email = forms.EmailField(label='邮箱')
+
+class UserFormForLogin(forms.Form):
+    username = forms.CharField(label='用户名',max_length=50)
+    password = forms.CharField(label='密码',widget=forms.PasswordInput())
+
+def regist(request):
+    if request.method == 'POST':
+        userform = UserFormForRegist(request.POST)
+        if userform.is_valid():
+            username = userform.cleaned_data['username']
+            password = userform.cleaned_data['password']
+            email = userform.cleaned_data['email']
+
+            newUser = theUser(user_name=username,user_password=password,user_email=email)
+            newUser.save()
+
+            return HttpResponse('regist success!!!')
+    else:
+        userform = UserFormForRegist()
+    return render(request,'regist.html',{'userform':userform})######
+
+def login(request):
+    if request.method == 'POST':
+        userform = UserFormForLogin(request.POST)
+        if userform.is_valid():
+            username = userform.cleaned_data['username']
+            password = userform.cleaned_data['password']
+
+            user = theUser.objects.filter(user_name__exact=username,user_password__exact=password)
+
+            if user:
+                return render(request,'index.html',{'userform':userform})
+            else:
+                return HttpResponse('用户名或密码错误,请重新登录')
+
+    else:
+        userform = UserFormForLogin()
+    return render(request,'login.html',{'userform':userform})
