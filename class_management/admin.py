eb026e3f014cdd7465410b48f2379aee84fb11f9
LyuJ1998 <31683380+LyuJ1998@users.noreply.github.com>
2017-11-08 08:33:14 +0800

    Update admin.py

--- class_management/admin.py
+++ class_management/admin.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Course,theUser
 
-admin.site.register(Course)
+#admin.site.register(Course)
 admin.site.register(theUser)
