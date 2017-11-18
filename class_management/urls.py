b53b5311b46eeccf00094d364d7d567bad357fc7
Xrosheart <31621210+YZ-WANG@users.noreply.github.com>
2017-11-18 21:26:35 +0800

    Update urls.py

--- class_management/urls.py
+++ class_management/urls.py
@@ -6,7 +6,13 @@ urlpatterns = [
     url(r'^login/$',views.login),
     url(r'^regist/$',views.regist),
     url(r'^edit_product/$',views.edit_product),
-    
+    url(r'^$', views.course_list, name='course_list'),
+    url(r'^(?P<category_slug>[-\w]+)/$',
+        views.course_list,
+        name='course_list_by_category'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        views.course_detail,
+        name='course_detail'),
     ]
 '''
 zhangzongrui and xiefei add the edit_product function
