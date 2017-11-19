bdce032a8e30a77a13fe91e66fb077848880eaae
Xrosheart <31621210+YZ-WANG@users.noreply.github.com>
2017-11-19 20:36:44 +0800

    Update urls.py

--- addclass/urls.py
+++ addclass/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.course_list, name='course_list'),
+    url(r'^search/$', views.search, name='search'),
     url(r'^(?P<category_slug>[-\w]+)/$',
         views.course_list,
         name='course_list_by_category'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
         views.course_detail,
         name='course_detail'),
-    url(r'^search/$', views.search, name='search'),
 ]
