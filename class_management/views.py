8f1cb85e7f36bca5eab48b7d0d372e6014dd2039
LyuJ1998 <31683380+LyuJ1998@users.noreply.github.com>
2017-11-26 11:28:46 +0800

    Update views.py

--- class_management/views.py
+++ class_management/views.py
@@ -8,24 +8,14 @@ from django.template import RequestContext, loader
 from django.shortcuts import redirect
 
 
-
 class UserFormForRegist(forms.Form):
-    username = forms.CharField(label='用户名',max_length=50,min_length=2,required=True)
-    password = forms.CharField(label='密码',widget=forms.PasswordInput())
-    email = forms.EmailField(label='邮箱',error_messages={'required': '邮箱不能为空'})
+    username = forms.CharField(label='用户名',max_length=50)
+    password = forms.CharField(label='密  码',widget=forms.PasswordInput())
+    email = forms.EmailField(label='邮  箱')
 
 class UserFormForLogin(forms.Form):
     username = forms.CharField(label='用户名',max_length=50)
-    password = forms.CharField(label='密码',widget=forms.PasswordInput())
-
-class UserFormForedit(forms.Form):
-    username = forms.CharField(label='用户名',max_length=50,min_length=2,required=True)
-    password = forms.CharField(label='密码',widget=forms.PasswordInput())
-    email=forms.EmailField(label='邮箱')
-    newpassword=forms.CharField(label='新密码',widget=forms.PasswordInput())
-class UserForminformation(forms.Form):
-    username=theUser.user_name
-    email=theUser.user_email
+    password = forms.CharField(label='密  码',widget=forms.PasswordInput())
 
 
 def regist(request):
@@ -35,11 +25,9 @@ def regist(request):
             username = userform.cleaned_data['username']
             password = userform.cleaned_data['password']
             email = userform.cleaned_data['email']
-
             newUser = theUser(user_name=username,user_password=password,user_email=email)
             newUser.save()
-
-            return HttpResponse('regist success!')
+            return render(request, 'index.html', {'userform': userform})
     else:
         userform = UserFormForRegist()
     return render(request,'regist.html',{'userform':userform})######
@@ -54,6 +42,7 @@ def login(request):
             user = theUser.objects.filter(user_name__exact=username,user_password__exact=password)
 
             if user:
+                request.session['User'] = username
                 return render(request,'index.html',{'userform':userform})
             else:
                 return HttpResponse('用户名或密码错误,请重新登录')
@@ -61,34 +50,20 @@ def login(request):
     else:
         userform = UserFormForLogin()
     return render(request,'login.html',{'userform':userform})
+
 def edit_product(request):
+    User = theUser.objects.get(user_name = request.session.get('User'))
+    class UserFormForedit(forms.Form):
+        email = forms.EmailField(label='邮  箱', initial=User.user_email)
     if request.method == 'POST':
         userform = UserFormForedit(request.POST)
         if userform.is_valid():
-            username = userform.cleaned_data['username']
-            password = userform.cleaned_data['password']
             email=userform.cleaned_data['email']
-            newpassword = userform.cleaned_data['newpassword']
-
-            user=theUser.objects.filter(user_name__exact=username,user_password__exact=password,user_email__exact=email)
-            if user:
-                newUser = theUser(user_name=username, user_password=newpassword)
-                newUser.save()
-                return HttpResponse('修改成功')
+            if User:
+                User.user_email = email
+                User.save()
             else:
-                return HttpResponse('密码错误不能修改')
+                return HttpResponse('请先登录!!!')
     else:
         userform = UserFormForedit()
     return render(request,'edit.html',{'userform':userform})######
-'''
-zhangzongrui and xiefei add the edit_product function and class UserFormForedit
-the function is used to edit the information of users
-'''
-
-    
-    
-    
-            
-
-            
-    
