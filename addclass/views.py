e45389d8dac209b0038db961a9dd040e0e40ba3c
Xrosheart <31621210+YZ-WANG@users.noreply.github.com>
2017-11-28 21:42:52 +0800

    Update views.py
    新增了choose方法来选课，以及detail方法里往session加入“course”

--- addclass/views.py
+++ addclass/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Course
+from class_management.models import theUser
 
 
 def course_list(request, category_slug=None):
@@ -22,6 +23,7 @@ def course_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    request.session['course'] = course.id
     return render(request,
                   'coursepage/course/detail.html',
                   {'course': course})
@@ -37,3 +39,12 @@ def search(request):
     course_list = Course.objects.filter(title__icontains=q)
     return render(request, 'coursepage/list.html', {'error_msg': error_msg,
                   'course_list': course_list})
+
+def choose(request):
+    thisCourse = Course.objects.get(id = request.session.get('course'))
+    User = theUser.objects.get(user_name = request.session.get('User'))
+    User.course_list.add(thisCourse)
+    User.save()
+    return render(request,
+                  'coursepage/course/detail.html',
+                  {'course': thisCourse})
