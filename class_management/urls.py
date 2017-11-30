1be7ea33354b8af156333abee60b2c642ce12c1f
LyuJ1998 <31683380+LyuJ1998@users.noreply.github.com>
2017-11-30 20:38:07 +0800

    Update urls.py

--- class_management/urls.py
+++ class_management/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.login),
+    url(r'^login/$',views.login),
     url(r'^regist/$',views.regist),
     url(r'^edit_product/$',views.edit_product),
-    url(r'^course/$', views.course_list, name='course_list'),
+    url(r'^$', views.course_list, name='course_list'),
     url(r'^(?P<category_slug>[-\w]+)/$',
         views.course_list,
         name='course_list_by_category'),
